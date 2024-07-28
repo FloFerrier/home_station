608bb8a8ba68da8b40534458378b841068a16f8d
flfer <florian.ferrier@aol.fr>
2024-07-28 18:41:23 +0200

    [service] Fix test suite for database module

--- service/src/database/database.py
+++ service/src/database/database.py
@@ -10,6 +10,7 @@ class Database:
             "org": "",
             "bucket": ""
         }
+        self.write_api = None
 
     def init(self, url: str = "", token: str = "", org: str = "", bucket: str = "") -> None:
         self.__api["url"] = "http://{}".format(url)
@@ -17,7 +18,7 @@ class Database:
         self.__api["org"] = org
         self.__api["bucket"] = bucket
         client = influxdb_client.InfluxDBClient(url=self.__api["url"], token=self.__api["token"], org=self.__api["org"])
-        self.__write_api = client.write_api(write_options=SYNCHRONOUS)
+        self.write_api = client.write_api(write_options=SYNCHRONOUS)
 
     def __data_check(self, data: dict) -> bool:
         keys = ["timestamp", "name", "location", "source", "type", "unit", "value"]
@@ -31,7 +32,10 @@ class Database:
         data_is_correct: bool = self.__data_check(data)
         if data_is_correct is True:
             record = influxdb_client.Point.from_dict(data, write_precision=WritePrecision.S, record_measurement_key="name", record_time_key="timestamp", record_tag_keys=["location", "source", "type", "unit"], record_field_keys=["value"])
-            self.__write_api.write(bucket=self.__api["bucket"], org=self.__api["org"], record=record)
+            try:
+                self.write_api.write(bucket=self.__api["bucket"], org=self.__api["org"], record=record)
+            except Exception as e:
+                data_is_correct = False
         return data_is_correct
 
 if __name__ == "__main__":
