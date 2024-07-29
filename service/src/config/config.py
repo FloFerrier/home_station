48a9d8be3e0f6d5ae5e321925fc6e6adafee8732
flfer <florian.ferrier@aol.fr>
2024-07-29 10:54:42 +0200

    [service] Add mock for removing config file persistant on test suite

--- service/src/config/config.py
+++ service/src/config/config.py
@@ -2,8 +2,13 @@ import yaml
 
 def config_read(filename: str = "") -> dict:
     config = dict()
-    with open(filename) as file:
+    try:
+        file = open(filename)
+    except Exception as e:
+        print(e)
+    else:
         config = yaml.safe_load(file)
+        file.close()
     return config
 
 if __name__ == "__main__":
--- service/tests/test_config.py
+++ service/tests/test_config.py
@@ -1,10 +1,35 @@
-import os
+import io
+import typing
+import yaml
 
 from src.config import config_read
 
-def test_Config():
-    test_filename = os. getcwd() + "/tests/test_config.yaml"
-    config = config_read(test_filename)
+def test_config_should_failed_due_to_wrong_filename(mocker) -> None:
+    mock_builtins_open = mocker.patch("builtins.open", side_effect=FileNotFoundError)
+
+    config = config_read("incorrect_filename")
+
+    mock_builtins_open.assert_called_with("incorrect_filename")
+
+def test_config_should_be_read_with_success(mocker) -> None:
+    def fake_open(path: str, *args, **kwargs) -> typing.TextIO:
+        return io.StringIO(yaml.dump(
+            {"openweathermap": {
+                "url": "openweathermap-url",
+                "city": "openweathermap-city",
+                "key": "openweathermap-key",
+                },
+             "influxdb": {
+                "url": "influxdb-url",
+                "token": "influxdb-token",
+                "org": "influxdb-org",
+                "bucket": "influxdb-bucket",
+                },
+            }))
+    mock_builtins_open = mocker.patch("builtins.open", new=fake_open)
+
+    config = config_read("filename")
+
     assert config is not None
     assert "openweathermap" in config is not None
     assert config["openweathermap"]["url"] == "openweathermap-url"
