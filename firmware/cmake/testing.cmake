ad34dd16d76df53fa8d6b1c4075038096eda3b05
flfer <florian.ferrier@aol.fr>
2024-08-29 11:53:13 +0200

    [firmware] Update file tree organization

--- firmware/cmake/testing.cmake
+++ firmware/cmake/testing.cmake
@@ -5,8 +5,15 @@ enable_testing()
 macro(add_unit_test unitestname)
     message(NOTICE "Create test executable: ${ARGV0}")
     add_executable(${ARGV0})
+    target_include_directories(${ARGV0} PRIVATE
+        ${CMAKE_SOURCE_DIR}/src
+        ${CMAKE_SOURCE_DIR}/src/console
+        ${CMAKE_SOURCE_DIR}/src/commands
+        ${CMAKE_SOURCE_DIR}/src/sensor
+        ${CMAKE_SOURCE_DIR}/src/shell
+        ${CMAKE_SOURCE_DIR}/test/mock
+    )
     target_sources(${ARGV0} PRIVATE ${ARGN})
-    target_include_directories(${ARGV0} PRIVATE ${CMAKE_SOURCE_DIR}/src ${CMAKE_SOURCE_DIR}/test/mock)
     target_compile_definitions(${ARGV0} PRIVATE -DTEST)
     target_compile_options(${ARGV0} PRIVATE -std=c99 -Og -g -Wall -W -Wextra)
     target_link_libraries(${ARGV0} PRIVATE cmocka-static bme68x)
