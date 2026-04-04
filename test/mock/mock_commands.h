3a1f0158d2162db1ce7cebab5dccec103d9984c2
Florian Ferrier <Florian.Ferrier@aol.fr>
2026-04-04 13:25:01 +0200

    feat(shell): remove commands module

--- test/mock/mock_commands.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_MOCK_COMMANDS_H
-#define TEST_MOCK_COMMANDS_H
-
-#include "commands.h"
-
-void mock_assert_call_command_getIndex(const char *cmd, command_index_e ret);
-void mock_assert_call_command_execute(command_index_e command_index, const uint32_t response_len_max, const char *expected_response);
-
-#endif  // TEST_MOCK_COMMANDS_H
