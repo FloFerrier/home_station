fd808459784ccb35d65cc312ea43cff1c017824d
Florian Ferrier <Florian.Ferrier@aol.fr>
2026-04-04 13:25:26 +0200

    feat(shell): integrate embedded-cli on shell module

--- src/shell/shell.c
+++ src/shell/shell.c
@@ -7,7 +7,10 @@
 #include <ctype.h>
 #include <string.h>
 
-#include "commands.h"
+#define EMBEDDED_CLI_IMPL
+#include "embedded_cli.h"
+
+#include "led.h"
 #include "console.h"
 
 #ifndef TEST
@@ -19,56 +22,49 @@ int loopCnt;
 #define STATIC
 #endif  // TEST
 
-#define COMMAND_STRING_LEN_MAX (255u)
-#define RING_BUFFER_LEN_MAX (255u)
-
-#define SHELL_RESPONSE_SIZE_MAX (2048u)
-
-typedef struct {
-    char buffer[RING_BUFFER_LEN_MAX + 1];
-    uint32_t index;
-} shell_s;
-STATIC shell_s shell = {0};
-
-STATIC bool character_isPrintable(char character) {
-    return (bool)isprint(character);
+static void writeChar(EmbeddedCli *embeddedCli, char c) {
+    (void)embeddedCli;
+    (void)console_send("%c", c);
 }
 
-STATIC bool character_isEndOfLine(char character) {
-    return (bool)(character == '\r' || character == '\n');
+static void setLed(EmbeddedCli *cli, char *args, void *context) {
+    (void) cli;
+    (void) context;
+
+    const char *arg1 = embeddedCliGetToken(args, 1);
+    if (strcmp(arg1, "on") == 0) {
+        led_setState(LED_ID_RED, LED_STATE_ON);
+    }
+    else if (strcmp(arg1, "off") == 0) {
+        led_setState(LED_ID_RED, LED_STATE_OFF);
+    }
+    else {
+        console_send("Incorrect arg ... \"on\" or \"off\" available !\r\n", arg1);
+    }
 }
 
 void shell_task(void *params) {
     (void)params;
 
-    (void)memset(shell.buffer, '\0', RING_BUFFER_LEN_MAX);
-    shell.index = 0u;
-    static char command_string[COMMAND_STRING_LEN_MAX + 1] = "";
+    EmbeddedCliConfig *config = embeddedCliDefaultConfig();
+    config->maxBindingCount = 16;
+    EmbeddedCli *cli = embeddedCliNew(config);
+    cli->writeChar = writeChar;
+    CliCommandBinding setLed_binding = {
+            .name = "set-led",
+            .help = "Set led on/off",
+            .tokenizeArgs = true,
+            .context = NULL,
+            .binding = setLed
+    };
+    embeddedCliAddBinding(cli, setLed_binding);
 
     do {
-        bool command_is_available = false;
-        uint32_t command_len = 0;
         char character = 0;
         bool reception_is_success = console_receive(&character);
         if (reception_is_success == true) {
-            if (character_isPrintable(character) == true) {
-                shell.buffer[shell.index++] = character;
-            } else if (character_isEndOfLine(character) == true) {
-                command_is_available = true;
-                shell.buffer[shell.index] = '\0';
-                command_len =
-                    (uint32_t)strnlen(shell.buffer, COMMAND_STRING_LEN_MAX);
-                (void)strncpy(command_string, shell.buffer, command_len + 1);
-                shell.index = 0u;
-            }
+            embeddedCliReceiveChar(cli, character);
         }
-        if (command_is_available == true) {
-            command_index_e command_index = command_getIndex(command_string);
-            static char response[SHELL_RESPONSE_SIZE_MAX] = "";
-            command_execute(command_index, SHELL_RESPONSE_SIZE_MAX - 1,
-                            response);
-            console_send(response);
-            command_is_available = false;
-        }
+        embeddedCliProcess(cli);
     } while (FOREVER());
 }
