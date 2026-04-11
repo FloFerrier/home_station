6d8fea17e6783438615387170afee61f50984007
Florian Ferrier <Florian.Ferrier@aol.fr>
2026-04-11 08:42:01 +0200

    refactor(protocol): remove unused protocol module

--- test/mock/mock_protocol.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_MOCK_PROTOCOL_H
-#define TEST_MOCK_PROTOCOL_H
-
-#include "protocol.h"
-
-void mock_assert_call_protocol_serialize(protocol_s protocol, const uint32_t string_len_max, const char *string);
-
-#endif  // TEST_MOCK_PROTOCOL_H
